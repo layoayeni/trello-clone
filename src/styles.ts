6161d72d0b37d1e915a6243ac93c35ce1f0929c7
layoayeni <omolayoayeni@gmail.com>
2023-03-07 03:20:30 +0100

    Created AddNewItem Component and added it to the App Component

--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components"
 
+type AddItemButtonProps = {
+    dark?: boolean
+}
+
 //AppContainer Component
 export const AppContainer = styled.div`
     align-items: flex-start;
@@ -37,4 +41,20 @@ export const CardContainer = styled.div`
     max-width: 300px;
     border-radius: 3px;
     box-shadow: #091e4240 0px 1px 0px 0px;
+`
+
+export const AddItemButton = styled.button<AddItemButtonProps>`
+    background-color: #ffffff3d;
+    border-radius: 3px;
+    border: none;
+    color: ${(props) => (props.dark ? "#000" : "#fff")};
+    cursor: pointer;
+    max-width: 300px;
+    padding: 10px 12px;
+    text-align: left;
+    transition: background 85ms ease-in;
+    width: 100%;
+    &:hover {
+        background-color: #ffffff52;
+    }
 `
